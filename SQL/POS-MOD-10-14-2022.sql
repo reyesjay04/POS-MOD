d59399c9b6f219f14eee59a19703650a85d39ec0
reyesjay04 <61256136+reyesjay04@users.noreply.github.com>
2022-10-14 17:44:23 +0800

    Script Update

--- SQL/POS-MOD-10-14-2022.sql
+++ SQL/POS-MOD-10-14-2022.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2022 at 11:09 AM
+-- Generation Time: Oct 14, 2022 at 11:43 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
